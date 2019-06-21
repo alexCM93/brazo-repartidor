e9db7555040238f5704156e6d7f88ef86e63a21d
alex.colmenares <alex.colmenares@AULESEEBE.UPC.EDU>
2019-06-21 18:16:50 +0200

    Move logic and replace variables for english ones

--- constants.py
+++ constants.py
@@ -4,18 +4,38 @@ Global constants
 import pygame
 
 # Colors
-NEGRO = (0, 0, 0)
-BLANCO = (255, 255, 255)
-VERDE = (0, 255, 0)
-ROJO = (255, 0, 0)
-colores = [BLANCO, VERDE, ROJO]
+black = (0, 0, 0)
+grey = (100, 100, 100)
+white = (255, 255, 255)
+green = (0, 255, 0)
+red = (255, 0, 0)
+colors = [grey, white, green, red]
 
+# Rectangle width
+rectangleWidth = 10
+rectangleHeight = 10
+
+framesPerSecond = 60
+
+# States
+menuState = 0
+mainState = 1
+resultsState = 2
+states = [menuState, mainState, resultsState]
 
 # Screen dimensions
 SCREEN_WIDTH = 1000
 SCREEN_HEIGHT = 750
 
-# images
+#
+beltWidth = 500
+containerWidth = 300
+
+# Initial position
+initialX = 90
+initialY = -50
+
+# Images
 menuBackground = pygame.image.load('images/menu.png')
 mainBackground = pygame.image.load('images/main.png')
 resultsBackground = pygame.image.load('images/results.png')
--- main.py
+++ main.py
@@ -4,92 +4,98 @@ import random
 
 pygame.init()
 
-# datos de pantalla
-dimensiones = [SCREEN_WIDTH, SCREEN_HEIGHT]
-pantalla = pygame.display.set_mode(dimensiones)
+# Screen data
+dimensions = [SCREEN_WIDTH, SCREEN_HEIGHT]
+screen = pygame.display.set_mode(dimensions)
 pygame.display.set_caption("Brazo repartidor")
 
-# Iteramos hasta que el usuario haga click sobre el botón de cerrar
-hecho = False
+# Main loop flag control
+run = True
 
-# Usado para gestionar cuán rápido se actualiza la pantalla
-reloj = pygame.time.Clock()
+# Game clock
+clock = pygame.time.Clock()
 
-# Posición de partida del rectángulo
-rect_x = 90
-rect_y = -50
+# State variable
+state = states[0]
 
-# Velocidad y dirección inicial del rectángulo
-rect_cambio_x = 0
-rect_cambio_y = 5
+# Current speed and position
+currentSpeed_X = 0
+currentSpeed_Y = 5
+pos_x = initialX
+pos_y = initialY
 
-color = random.choice(colores)
-cantidad = 1
+color = random.choice(colors)
+remainingItems = 2
 
 background = mainBackground
-# ----------------------- BUCLE PRINCIPAL-------------------------------------------------#
 
-while not hecho:
-    for evento in pygame.event.get():  # El usuario hizo algo
-        if evento.type == pygame.QUIT:  # Si el usuario hace click sobre cerrar
-            hecho = True  # Marca que ya lo hemos hecho, de forma que abandonamos el bucle
+# ==================== MAIN LOOP ==================== #
 
-    pantalla.blit(background, (0, 0))
-    # --- Lógica del juego
-    # Mueve el punto de partida del rectángulo
-    rect_x += rect_cambio_x
-    rect_y += rect_cambio_y
+while run:
+    # Define FPS
+    clock.tick(framesPerSecond)
 
-    # Rebota el rectángulo, si hace falta.
+    # Empty screen
+    screen.fill(black)
 
-    if rect_y > 340 and rect_x == 90 :  # Si el valor de y es mayor a 450 y menor a 0 píxeles,
-        rect_cambio_y = 0  # modifico la tasa de cambio, de positiva, a negativa.
-        rect_cambio_x = 5
+    # End program if user quits game
+    for events in pygame.event.get():
+        if events.type == pygame.QUIT:
+            run = False
 
-    if rect_x > 500 and color == BLANCO :  # Si el valor de y es mayor a 450 y menor a 0 píxeles,
-        rect_cambio_y = -4  # modifico la tasa de cambio, de positiva, a negativa.
-        rect_cambio_x = 5
-    if rect_x > 500 and color == VERDE:  # Si el valor de y es mayor a 450 y menor a 0 píxeles,
-        rect_cambio_y = 4  # modifico la tasa de cambio, de positiva, a negativa.
-        rect_cambio_x = 5
-    if rect_x > 500 and color == ROJO:  # Si el valor de y es mayor a 450 y menor a 0 píxeles,
-        rect_cambio_y = 2  # modifico la tasa de cambio, de positiva, a negativa.
-        rect_cambio_x = 5
-    if rect_x > 800 :  # Si el valor de y es mayor a 450 y menor a 0 píxeles,
-        rect_cambio_y = 0  # modifico la tasa de cambio, de positiva, a negativa.
-        rect_cambio_x = 5
-    if rect_x > 1000 and cantidad > 0:  # Si el valor de y es mayor a 450 y menor a 0 píxeles,
-        if cantidad == 1:
-           rect_cambio_y = 0  # modifico la tasa de cambio, de positiva, a negativa.
-        else:
-           rect_cambio_y = 5
-        rect_cambio_x = 0
-        rect_x = 90
-        rect_y = -50
-        color = random.choice(colores)
-        print (cantidad)
-        cantidad = cantidad - 1
-    if cantidad == 0:
+    if state == states[1]:
+        continue
+
+    screen.blit(background, (0, 0))
+
+    # == Move rectangle depending on condition == #
+
+    if pos_y > 340 and pos_x == 90:
+        currentSpeed_Y = 0
+        currentSpeed_X = 5
+
+    # Item is inside distributor
+    if pos_x > beltWidth:
+        if color == white:
+            currentSpeed_Y = -4
+        if color == green:
+            currentSpeed_Y = 4
+        if color == red:
+            currentSpeed_Y = 2
+        if color == grey:
+            currentSpeed_Y = -2
+
+    # Item is in container
+    if pos_x > beltWidth + containerWidth:
+        currentSpeed_Y = 0  
+
+    # Item is out of screen
+    if pos_x > SCREEN_WIDTH:
+        currentSpeed_Y = 5
+        currentSpeed_X = 0
+        pos_x = initialX
+        pos_y = initialY
+        color = random.choice(colors)
+        print (remainingItems)
+        remainingItems = remainingItems - 1
+
+    pos_x += currentSpeed_X
+    pos_y += currentSpeed_Y
+    
+    # Show results page when 
+    if remainingItems == 0:
         background = resultsBackground
+        state = states[1]
 
+    # Draw background
+    screen.blit(background, (0, 0))
 
-    # --- Dibujamos
-    # Limpia la pantalla y establece su color de fondo
-    #pantalla.fill(constants.NEGRO)
-    pantalla.blit(background, (0, 0))
+    # Draw rectangles
+    pygame.draw.rect(screen, black, [pos_x, pos_y, 50, 50])
+    pygame.draw.rect(screen, color, [pos_x + rectangleWidth, pos_y + rectangleHeight, 30, 30])
 
-    # Dibujamos los Rectángulos
-    pygame.draw.rect(pantalla, NEGRO, [rect_x, rect_y, 50, 50])
-    pygame.draw.rect(pantalla, color, [rect_x + 10, rect_y + 10, 30, 30])
-
-    # --- Envolvemos todo
-    # Limitamos a 60 fotogramas por segundo
-    reloj.tick(60)
-
-    # Avancemos y actualicemos la pantalla con lo que hemos dibujado.
+    # Update screen
     pygame.display.flip()
 
-
-# Pórtate bien con el IDLE. Si nos olvidamos de esta línea, el programa se 'colgará'
-# en la salida.
+# Close pygame library instance
 pygame.quit()
